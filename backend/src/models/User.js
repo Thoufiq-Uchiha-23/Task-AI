32f317de54b662e277e6c6a53575543ec2f803bb
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-05 12:42:29 +0530

    User schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+name;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Hash password converting
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
+});
+
+// Validate password
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
